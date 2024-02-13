fa9672fac39b9a553c545553c08cf28d74af28c4
Samer Alkarkoukly <54069692+alkarkoukly@users.noreply.github.com>
2024-02-14 00:25:05 +0100

    adding icecream exercise

---
+++ 9-exercises/index.js
@@ -0,0 +1,22 @@
+const place = "World";
+const greeting = "Hello";
+
+//console.log('Hello World! 😜');
+//console.log("%s, %s, ", greeting, place);
+console.log(`${greeting}, ${place}!`)
+
+
+
+/*
+-----------------------
+    Comments Block
+-----------------------
+*/
+
+console.log(var_declare)
+// console.log(let_declare)
+
+var var_declare = "var declaration"
+let let_declare = "let declaration"
+
+console.log(var_declare)
